e437ae50645a8c5ee59175002bf6bcc889bc66f3
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-04-23 14:41:27 +0300

    Adds root html and js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import listItem from "./classes/list-item";
+import project from "./classes/project";
+
+const test = new listItem("h", "h", "h", "h");
+const defaultProject = new project('default');
+
+console.log(defaultProject);
