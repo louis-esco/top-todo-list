e4074bb2caa06eb9a3bedc38449b368703212a6c
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-04-23 14:59:05 +0300

    Changes file name

--- src/classes/todo-item.js
+++ src/classes/todo-item.js
@@ -1,9 +1,13 @@
-export default class listItem {
+export default class todoItem {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.done = false;
+    }
+
+    markDone() {
+        this.done = true;
     }
 }
