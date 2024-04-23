247b9c1bdc6140ff933fecf396a4957f06ab52c6
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-04-23 14:41:42 +0300

    Adds basic classes listitem and project

---
+++ src/classes/list-item.js
@@ -0,0 +1,9 @@
+export default class listItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.done = false;
+    }
+}
---
+++ src/classes/project.js
@@ -0,0 +1,6 @@
+export default class project {
+    constructor(name) {
+        this.name = name;
+        this.items = [];
+    }
+}
