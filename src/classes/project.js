d1f60c27d88204d5bf70974710a6cbed96e2bd7e
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-04-23 18:55:06 +0300

    Implements UUID on todoitems & projects

--- src/classes/project.js
+++ src/classes/project.js
@@ -1,15 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class project {
     constructor(name) {
         this.name = name;
-        this.items = [];
-    }
-
-    addItem(item) {
-        this.items.push(item);
-    }
-
-    removeItem(item) {
-        const itemIndex = this.items.map(i => i.title).indexOf(item);
-        this.items.splice(itemIndex, 1);
+        this.id = uuidv4();
     }
 }
--- src/classes/todo-item.js
+++ src/classes/todo-item.js
@@ -1,10 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class todoItem {
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, dueDate, priority, project = "default") {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.project = project
         this.done = false;
+        this.id = uuidv4();
     }
 
     markDone() {
